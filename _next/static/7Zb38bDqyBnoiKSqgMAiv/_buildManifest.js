2d80cd7af015a0e2ca35f593e9d12afd955f0ced
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2026-01-19 16:07:27 +0300

    first commit

---
+++ _next/static/7Zb38bDqyBnoiKSqgMAiv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-7d75bff98c4f8336.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
